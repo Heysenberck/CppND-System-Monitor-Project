4c82230c11a023880082cfa86fcbe828913926b8
chti <chrtimm@web.de>
2021-04-11 20:01:04 +0200

    add implemtation of cpu utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,46 @@
 #include "processor.h"
+#include "linux_parser.h"
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include "linux_parser.h"
+
+using std::vector;
+using std::string;
+
+using namespace LinuxParser;
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  prevCpuTimeUnits_ = currCpuTimeUnits_;
+
+  vector<string> cpuTimeUnits = LinuxParser::CpuUtilization();
+
+  float user       = std::stoi(cpuTimeUnits[CPUStates::kUser_]);
+  float nice       = std::stoi(cpuTimeUnits[CPUStates::kNice_]);
+  float system     = std::stoi(cpuTimeUnits[CPUStates::kSystem_]);
+  float idle       = std::stoi(cpuTimeUnits[CPUStates::kIdle_]);
+  float iowait     = std::stoi(cpuTimeUnits[CPUStates::kIOwait_]);
+  float irq        = std::stoi(cpuTimeUnits[CPUStates::kIRQ_]);
+  float softirq    = std::stoi(cpuTimeUnits[CPUStates::kSoftIRQ_]);
+  float steal      = std::stoi(cpuTimeUnits[CPUStates::kSteal_]);
+  float guest      = std::stoi(cpuTimeUnits[CPUStates::kGuest_]);
+  float guest_nice = std::stoi(cpuTimeUnits[CPUStates::kGuestNice_]);
+
+  prevIdle_ = currIdle_;
+  currIdle_ = idle + iowait;
+
+  prevNonIdle_ = currNonIdle_;
+  currNonIdle_ = user + nice + system + irq + softirq + steal;
+
+  float prevTotal = prevIdle_ + prevNonIdle_;
+  float currTotal = currIdle_ + currNonIdle_;
+
+  // differentiate: actual value minus the previous one
+  float totald = currTotal - prevTotal;
+  float idled  = currIdle_ - prevIdle_;
+
+  return (totald - idled)/totald;
+}
