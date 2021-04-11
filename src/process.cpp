a20fcb72d6b354e60047dfa50e086f657d588fab
chti <chrtimm@web.de>
2021-04-11 19:51:12 +0200

    commit the changes to process class

--- src/process.cpp
+++ src/process.cpp
@@ -10,24 +10,54 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+void Process::Pid(int processId) {
+  pid_ = processId;
+}
+
+int Process::Pid() const { 
+  return pid_;
+}
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+float Process::CpuUtilization() const {
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+  return 0;
+}
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+void Process::Command(const std::string &cmd) {
+  command_ = cmd;
+}
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::Command() const { 
+  return command_;
+}
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+void Process::Ram(const std::string &mem) {
+  ram_ = mem;
+}
+
+string Process::Ram() const {
+  return ram_;
+}
+
+void Process::User(const std::string &usr) {
+  user_ = usr;
+}
+
+string Process::User() const {
+  return user_;
+}
+
+void Process::UpTime(long int time) {
+  uptime_ = time;
+}
+
+long int Process::UpTime() const {
+  return uptime_;
+}
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+bool Process::operator<(Process const& a) const {
+  return ram_ < a.ram_;
+}
