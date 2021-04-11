a9ac63a68ab389bd3a288d0a6c534c984d853204
chti <chrtimm@web.de>
2021-04-11 19:49:01 +0200

    update the Process class interface and add some member variables

--- include/process.h
+++ include/process.h
@@ -7,17 +7,26 @@ Basic class for Process representation
 It contains relevant attributes as shown below
 */
 class Process {
- public:
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
+public:
+  void Pid(int id);
+  int  Pid() const;
+  void User(const std::string &usr);
+  std::string User() const;                // TODO: See src/process.cpp
+  void Command(const std::string &cmd);
+  std::string Command() const;
+  float CpuUtilization() const;            // TODO: See src/process.cpp
+  void Ram(const std::string &mem);
+  std::string Ram() const;
+  void UpTime(long int time);
+  long int UpTime() const;                 // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
 
-  // TODO: Declare any necessary private members
- private:
+private:
+  int pid_;
+  std::string command_;
+  std::string ram_;
+  std::string user_;
+  long int uptime_;
 };
 
 #endif
